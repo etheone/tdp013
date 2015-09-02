516007aa0d763cac61d6d2c34c1cf8cdfaedbc4a
Emil Nilsson <emini901@student.liu.se>
2015-09-02 12:37:20 +0200

    Finished html, css and script.

--- lab1/js/scripts.js
+++ lab1/js/scripts.js
@@ -1,25 +1,63 @@
 messagesDiv = document.getElementbyId("messagesDiv");
+SCREEN_HEIGHT = screen.height;
+messagesCount = 0;
 
 function postMessage() {
     
-    alert(document.getElementById("newMessage").value);
     //return document.getElementById("newMessage").value;
-    addNewMessage(document.getElementById("newMessage").value);
+    var text = document.getElementById("newMessage").value;
+    if (valid(text)) {
+	addNewMessage(text);
+    } else {
+	textError();
+    }
+    eraseText();
+	
 }
 
 
 function addNewMessage(text) {
-    var message = document.createElement('div');
-    message.className = "message";
-    message.innerHTML = text;
-    messagesDiv[0].appendChild(iDiv);
+    document.getElementById("errorText").style.display = "none";
+    var newDiv = document.createElement('div');
+    var divHtml = text + "<input id='read' value='Mark as read' type='checkbox' onclick='markAsRead(this)'>";
+
+ 
+    //var messageid = "message" + messageCount;
+
+    //messagesCount = messagesCount + 1;
+
+  //  newDiv.idName = messageid;
+    newDiv.className = "unread";
     
-
-
-
+    newDiv.innerHTML = divHtml;
+    messagesDiv.appendChild(newDiv);
     
-
-
-
-   
 }
+
+function markAsRead(name) {
+    
+    if (name.parentElement.className == "read") {
+        name.parentElement.className = "unread";
+    } else {
+	name.parentElement.className = "read";
+    }
+    
+}
+
+function valid(text) {
+    if (text.length > 140 || text.length < 1) {
+	return false;
+    }
+    return true;
+}
+
+function textError() {
+    
+    document.getElementById("errorText").style.display = "block";
+    
+}
+
+function eraseText() {
+    document.getElementById("newMessage").value = "";
+}
+
