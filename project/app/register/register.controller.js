d322204ccc65c412b9b602bbe0be4998106ff1e8
Emil Nilsson <emini901@student.liu.se>
2015-09-30 13:28:31 +0200

    Oh god

---
+++ project/app/register/register.controller.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
+    function RegisterController(UserService, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+		 
+                    if (response.success) {
+			alert("success!");
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/login');
+                    } else {
+			alert("error");
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+
+})();
