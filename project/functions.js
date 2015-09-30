071ad729ffbc987a76aaaf91bba17d19c72840b5
Emil Nilsson <emini901@student.liu.se>
2015-09-30 13:32:56 +0200

    added functions

---
+++ project/functions.js
@@ -0,0 +1,134 @@
+/********************************************
+             USERDB FUNCTIONS
+*********************************************/
+
+function createUser(req, res, user, usersdb) {
+    //Check if users exists else create user
+    console.log(user['firstName']);
+    console.log(user['lastName']);
+    console.log(user['username']);
+    console.log(user['password']);
+    console.log(user);
+    try {
+	usersdb.insert({username: user['username'], firstName: user['firstName'], lastName: user['lastName'], pwd: user['password'], friends: [ ], requests: [ ]});
+	console.log("new user created");
+    } catch(err) {
+	console.log("There was an error creating the user: " + err);
+    }
+    
+}
+
+function login(req, res, user, usersdb) {
+    //var docs = searchDb(usersdb, 'username', user['username']);
+    usersdb.find({'username':user['username']}, function(err, docs) {
+	try {
+	    if(docs.length > 0) {
+		console.log(docs[0]);
+		if ( docs[0]['pwd'] == user['password'] ) {
+		    console.log("User found and password correct");
+		} else {
+		    console.log("User found but password incorrect");
+		}
+	    } else {
+		console.log("Sumthing wrong user not found");
+	    }
+	    console.log("Success!");
+	    console.log(docs);
+	    res.status(200).json(docs);
+	} catch(err) {
+	    res.status(500);
+	}
+    });
+}
+
+function isUser(req, res, user, usersdb) {
+    var userToCheck = user['username'];
+
+    
+} 
+
+function searchDb(db, key, value) {
+    console.log(db);
+    db.find({key:value}, function(err, docs) {
+	return docs;
+/*	try {
+	    res.status(200).json(docs);
+	} catch(err) {
+	    res.status(500);
+	}*/
+    });
+    
+}
+
+		
+/*function getAllMessages(req, res, db) {
+
+    db.find({}, function (err, docs) {
+	try {
+	    res.status(200).json(docs);
+	} catch(err) {
+	    res.status(500);
+	}
+    })
+}
+
+function saveMessage(req, res, db, message) {
+
+    if (message != null && validMessage(message)) {
+	if (postMessage(message, db)) {
+	    return 200;
+	} else {
+	    return 500;
+	}
+    } else {
+	return 400;
+    }
+}
+
+function flagMessage(req, res, db, url) {
+    
+    var urlParts = url.parse(req.url, true);
+    messageToFlag = urlParts.query['id'];
+    
+    try {
+    db.find({'_id':messageToFlag}, function(err, docs) {
+	if (messageToFlag != null && docs.length > 0 ) {
+
+		db.findAndModify({ _id: messageToFlag }, { $set: {flag: true} });
+		res.sendStatus(200);
+
+	}  else {
+	    res.sendStatus(400);
+	}
+	
+    });
+    } catch(err) {
+	res.sendStatus(400);
+    }
+}
+
+function postMessage(messageToAdd, db) {
+    try {
+	db.insert({message: messageToAdd, flag: false});
+	return true;
+    } catch(err) {
+	console.log("There was an error inserting the message: " + err);
+	return false;
+    } 
+}
+
+function validMessage(message) {
+    if (message.length > 140 || message.length == 0) {
+	return false;
+    }
+    return true;
+        
+}
+
+exports.saveMessage = saveMessage;
+exports.flagMessage = flagMessage;
+exports.getAllMessages = getAllMessages;
+*/
+
+exports.createUser = createUser;
+exports.login = login;
