e8a7d3e0b1f12074602968839b81ec73a81d390a
Emil Nilsson <emini901@student.liu.se>
2015-10-06 11:03:11 +0200

    Most of the project rebuilt and directory structure changed, added massive changes to project

---
+++ tdp013_projekt/app.js
@@ -0,0 +1,179 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var session = require('express-session');
+var Negotiator = require('negotiator');
+var errorHandler = require('errorhandler');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
+
+var functions = require('./functions.js');
+
+var DBconfig = require('./db.js');
+mongoose.connect(DBconfig.url);
+
+var User = require('./user');
+
+//==================================================================
+// Define the strategy to be used by PassportJS
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+	if (username === "admin" && password === "admin") // stupid example
+	    return done(null, {name: "admin"});
+
+	return done(null, false, { message: 'Incorrect username.' });
+    }
+));
+
+passport.use('local-login', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+},function(req, username, password, done) {
+    process.nextTick(function() {
+	User.findOne({'local.username': username }, function(err, user){
+	    if(err)
+		return done(err);
+	    if(!user)
+		return done(null, false, { message: 'Incorrect username' });
+	    if(user.local.password != password)
+		return done(null, false, { message: 'Incorrect password' });
+	    return done(null, user);
+	})
+    })
+}));
+
+passport.use('local-signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+    
+}, function(req, username, password, done) {
+    process.nextTick(function() {
+//	console.log(req);
+	var newUser = new User();
+	newUser.local.username = username;
+	newUser.local.password = password;
+	newUser.local.firstName = req.body.firstname;
+	newUser.local.lastName = req.body.lastname;
+	console.log(newUser.local.username);
+	console.log(newUser.local.password);
+	console.log(newUser.local.firstName);
+	console.log(newUser.local.lastName);
+	functions.createUser(newUser);
+	return done(null, newUser);
+    })
+}));
+
+// Serialized and deserialized methods when got from session
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+// Define a middleware function to be used for every secured routes
+var auth = function(req, res, next){
+    console.log(req.isAuthenticated());
+  if (!req.isAuthenticated()) 
+  	res.send(401);
+  else
+  	next();
+};
+//==================================================================
+
+// Start express application
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+app.use(morgan('dev'));
+app.use(cookieParser()); 
+app.use(bodyParser.json());
+//app.use(express.methodOverride());
+app.use(session({
+    secret: 'securedsession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection })
+}));
+
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+//==================================================================
+// routes
+app.get('/', function(req, res){
+  res.render('index', { title: 'Express' });
+});
+
+app.get('/users', auth, function(req, res){
+    var usersArr = [];
+    User.find({}, function(err, users) {
+	if(err) {
+	    console.log(err);
+	    throw err;
+	} else {
+	    //console.log(users[2].local.firstName);
+	    for(var x in users) {
+		
+		var temp = {};
+		temp['name'] = (users[x].local.firstName + " " + users[x].local.lastName);
+		//console.log(temp);
+	//	console.log(users[x].local.firstName);
+		usersArr.push(temp);
+	    }
+	}
+	
+	res.send(usersArr);
+    });
+    
+ // res.send([{name: "user1"}, {name: "user2"}]);
+});
+//==================================================================
+
+//==================================================================
+// route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+    console.log(req.body);
+  res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+app.post('/register', passport.authenticate('local-signup'), function(req, res) {
+    res.send(req.user);
+});
+
+// route to log in
+app.post('/login', passport.authenticate('local-login'), function(req, res) {
+  res.send(req.user);
+});
+
+// route to log out
+app.post('/logout', function(req, res){
+  req.logOut();
+  res.send(200);
+});
+//==================================================================
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ tdp013_projekt/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    local: {
+	username: String,
+	password: String,
+	firstName: String,
+	lastName: String,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
